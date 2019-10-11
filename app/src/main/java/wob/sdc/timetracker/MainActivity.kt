c049f8eb07295446b5847ea4e6e308950ba6d42a
IIandiI <iandi@Crash-Bandicoot.local>
2019-10-11 15:43:51 +0200

    add basic timer function

--- app/src/main/java/wob/sdc/timetracker/MainActivity.kt
+++ app/src/main/java/wob/sdc/timetracker/MainActivity.kt
@@ -5,26 +5,67 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Chronometer
 import android.widget.TextView
+import android.os.SystemClock
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var chronometer: Chronometer
+
+    private var todaysHours: Int = 0
+    private var todaysMinutes: Int = 0
+
+    private var runningHours: Int = 0
+    private var runningMinutes: Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        this.chronometer = findViewById(R.id.timer)
+        this.chronometer.text = "00:00:00"
+           this.chronometer.setOnChronometerTickListener { chrono -> onChronometerTick(chrono) }
 
         findViewById<Button>(R.id.startTimer).setOnClickListener { startTimer() }
         findViewById<Button>(R.id.stopTimer).setOnClickListener { stopTimer() }
 
     }
 
+    fun onChronometerTick(chrono: Chronometer) {
+        val time = SystemClock.elapsedRealtime() - chrono.base
+        val h = (time / 3600000).toInt()
+        val m = (time - h * 3600000).toInt() / 60000
+        val s = (time - (h * 3600000).toLong() - (m * 60000).toLong()).toInt() / 1000
+        val hh = if (h < 10) "0$h" else h.toString() + ""
+        val mm = if (m < 10) "0$m" else m.toString() + ""
+        val ss = if (s < 10) "0$s" else s.toString() + ""
+        chrono.text = "$hh:$mm:$ss"
+
+        this.runningHours = Integer.parseInt(hh)
+        this.runningMinutes = Integer.parseInt(mm)
+
+        printTodaysWork()
+    }
 
 
     fun startTimer(){
-        findViewById<Chronometer>(R.id.timer).start()
+        this.chronometer.base = SystemClock.elapsedRealtime()
+        this.chronometer.start()
     }
 
     fun stopTimer(){
-        findViewById<Chronometer>(R.id.timer).stop()
+        this.chronometer.stop()
+        this.todaysHours += this.runningHours
+        this.todaysMinutes += this.runningMinutes
+    }
+
+    fun printTodaysWork(){
+        val todaysWork = findViewById<TextView>(R.id.todaysWork)
+        val actualHours = this.todaysHours + this.runningHours
+        val actualMinutes = this.todaysMinutes + this.runningMinutes
+        todaysWork.setText("Todays work is " + actualHours + " hours and " + actualMinutes + " minutes")
+
+
     }
 }
