e263bcdd9861c2a75cd4cbdcaede50f4da8c161f
IIandiI <iandi@Crash-Bandicoot.local>
2019-10-11 14:54:34 +0200

    simple start stop timer

--- app/src/main/java/wob/sdc/timetracker/MainActivity.kt
+++ app/src/main/java/wob/sdc/timetracker/MainActivity.kt
@@ -2,11 +2,29 @@ package wob.sdc.timetracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Chronometer
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<Button>(R.id.startTimer).setOnClickListener { startTimer() }
+        findViewById<Button>(R.id.stopTimer).setOnClickListener { stopTimer() }
+
+    }
+
+
+
+    fun startTimer(){
+        findViewById<Chronometer>(R.id.timer).start()
+    }
+
+    fun stopTimer(){
+        findViewById<Chronometer>(R.id.timer).stop()
     }
 }
